5523cd1f02a593f2c5e255d2b822fa92ad07365d
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2022-12-19 20:54:59 +0300

    Created a package.json file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      
+      <footer>
+      <a href="https://github.com/Mugishaa77/Week-4/tree/master/public"
+      target = "_blank"
+      rel="noreferrer">Open-source by Mugisha</a>
+    </footer>
     </div>
   );
 }
